5fe3740f587aa96eee6722ddbf5a35247b4e3c58
YOGESH <yogrival911@gmail.com>
2021-05-13 18:34:16 +0530

    ShopAdmin

---
+++ app/src/main/java/com/yogdroidtech/shopadmin/MainActivity.java
@@ -0,0 +1,38 @@
+package com.yogdroidtech.shopadmin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+@BindView(R.id.button4)
+Button bannerButton;
+@BindView(R.id.button5)
+Button productsButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        bannerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, UploadActivity.class));
+            }
+        });
+        productsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, ProductUploadActivity.class));
+            }
+        });
+
+
+    }
+}
